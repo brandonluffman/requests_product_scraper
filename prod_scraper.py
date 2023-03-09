6cc2bc7f26e635642c6894cfd65527a2bb840f7e
Brandon Luffman <brandonluff10@gmail.com>
2023-03-08 20:58:41 -0500

    Product Page Scraper

--- prod_scraper.py
+++ prod_scraper.py
@@ -22,22 +22,27 @@ def get_source(url):
     except requests.exceptions.RequestException as e:
         print(e)
 
-# https://www.google.com/webhp?tbm=shop
-# https://shopping.google.com/?nord=1
-# https://shopping.google.com/search?q=
 def get_results(query):
     
+    urls = ['https://www.google.com/shopping/product/7414362887907499803?hl=en&psb=1&q=bose+quietcomfort+45&prds=eto:7699631281679971839_0,pid:5292486479779850642,rsk:PC_12836789135460115869&sa=X&ved=0ahUKEwjv9cGwic39AhWDADQIHaGVBjkQ8wII9gs', 'https://www.google.com/shopping/product/14556986932651510680?hl=en&q=sony+wh-1000xm5&prds=eto:17832498348390085438_0;16734284565725029642_0;7614542217126070872_0,pid:1061495521773557507,rsk:PC_11648784580466805463&sa=X&ved=0ahUKEwjGgJDMus39AhViFlkFHWWvAUYQ9pwGCBQ']
     query = urllib.parse.quote_plus(query)
-    response = get_source("https://www.google.com/search?tbm=shop&hl=en&psb=1&ved=0CAAQvOkFahcKEwj4lfLEmsv9AhUAAAAAHQAAAAAQJQ&q=" + query)
+    for url in urls:
+        response = get_source(url)
     
     return response
 
 def parse_results(response):
     
-    css_identifier_result = ".sh-pr__product-results-grid"
-    css_identifier_title = "h2"
-    css_identifier_link = "a"
-    css_identifier_text = "sh-t__title"
+    css_identifier_result = ".sg-product__dpdp-c"
+    css_product_img = ".wTvWSc img"
+    css_product_description = ".Zh8lCd p .sh-ds__full .sh-ds__full-txt"
+    css_product_specs = ".lW5xPd .crbkUb"
+    css_product_rating = ".QKs7ff .uYNZm"
+    css_product_reviews = "#-9110982622418926094-full"
+    css_product_reviews_title = ".XBANlb .P3O8Ne"
+    css_product_reviews_rating = ".nMkOOb .UzThIf"
+    css_product_review_count = ".QKs7ff .qIEPib"
+    css_product_purchasing = ".dOwBOc tr"
 
 
     results = response.html.find(css_identifier_result)
@@ -45,17 +50,20 @@ def parse_results(response):
     output = {}
     link_count = 0
 
-
-    # for result in results:
-    #     if link_count < len(queries):
-    #     # link_count += 1
-    #         output[f'Link #{link_count}'] = result.find(css_identifier_link, first=True).attrs['href'], result.find(css_identifier_title, first=True).text
-    #         link_count += 1
     for result in results:
-        # link_count += 1
-            link = 'https://www.google.com/' + result.find(css_identifier_link, first=True).attrs['href']
-            output[f'Link #{link_count}'] = link, result.find(css_identifier_link, first=True).text
-            link_count += 1
+        product_description = result.find(css_product_description, first=True).text
+        product_rating = result.find(css_product_rating, first=True).text
+        review_count = result.find(css_product_review_count, first=True).text
+        product_img = result.find(css_product_img, first=True)
+        product_specs = result.find(css_product_specs, first=True).text
+        product_reviews_title = result.find(css_product_reviews_title, first=True).text
+        product_reviews_rating = result.find(css_product_reviews_rating, first=True)
+        product_reviews = result.find(css_product_reviews, first=True)
+        product_purchasing = result.find(css_product_purchasing, first=True).text
+
+
+
+        output[f'Link #{link_count}'] = product_description, product_rating, review_count, product_img, product_specs, product_purchasing, product_reviews, product_reviews_title, product_reviews_rating
 
 
     return output
@@ -67,7 +75,7 @@ def google_search(query):
 
 # query = input("What would you like to get the links for? \n")
 
-queries = ['airpods', 'sony wh-1000xm5', 'bose quiet comfort 45']
+queries = ['https://www.google.com/shopping/product/7414362887907499803?hl=en&psb=1&q=bose+quietcomfort+45&prds=eto:7699631281679971839_0,pid:5292486479779850642,rsk:PC_12836789135460115869&sa=X&ved=0ahUKEwjv9cGwic39AhWDADQIHaGVBjkQ8wII9gs', 'https://www.google.com/shopping/product/14556986932651510680?hl=en&q=sony+wh-1000xm5&prds=eto:17832498348390085438_0;16734284565725029642_0;7614542217126070872_0,pid:1061495521773557507,rsk:PC_11648784580466805463&sa=X&ved=0ahUKEwjGgJDMus39AhViFlkFHWWvAUYQ9pwGCBQ']
 
 
 for quer in queries:
