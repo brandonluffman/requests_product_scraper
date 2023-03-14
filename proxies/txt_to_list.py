774c16149b1be220a550b88e9195061951541249
Brandon Luffman <brandonluff10@gmail.com>
2023-03-14 12:29:46 -0400

    file organization

--- proxies/txt_to_list.py
+++ proxies/txt_to_list.py
@@ -2,7 +2,7 @@ with open("proxies_list.txt") as f:
     content_list = f.readlines()
 
 # print the list
-print(content_list)
+# print(content_list)
 
 # remove new line characters
 content_list = [x.strip() for x in content_list]
